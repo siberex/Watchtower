acbf38227412dcacd58aaf0d689a02c7bebb1acd
Stephan Legachev <me@sib.li>
2011-09-15 16:30:48 +0900

    Added language detection function

--- app/helpers.js
+++ app/helpers.js
@@ -6,6 +6,7 @@ var dates = require("ringo/utils/dates");
 
 export(
   "baseUrl",
+  "getLang",
   "timeFormat"
 );
 
@@ -17,4 +18,69 @@ function baseUrl(name) {
 
 function timeFormat(date) {
   return dates.format(date, "HH:mm");
+}
+
+/**
+ * Get (or guess) the language of viewer.
+ * @param {Request} request
+ */
+function getLang(request) {
+  var langMap = {
+    "ru" : "ru",
+    "en" : "en",
+    "default" : "en" // default language
+  };
+  var countryMap = {
+    "RU" : "ru",
+    "BY" : "ru",
+    "UA" : "ru",
+    "ZZ" : langMap.default
+  };
+
+  // TODO: Get language was set by user directly (saved in cookie).
+
+  // Country to language conversion is not a good idea in most cases.
+  // Note that x-appengine-country is AppEngine-specific header.
+  var country = request.headers["x-appengine-country"];
+  var lang = false;
+
+  if (!country || country == "ZZ" || !countryMap[country]) {
+    var acceptLangs = request.headers["accept-language"];
+
+    /***
+    // Rhino JavaScript does not support look-behind assertion, so I use Java call here.
+    // http://www.regular-expressions.info/lookaround.html
+    // Replaced later with simplified RegExp in Javascript.
+    var re = "([a-z]{2})(?=-[a-z]{2})|(?<!-)([a-z]{2})(?=;|,)";
+    var pattern = java.util.regex.Pattern.compile(re, java.util.regex.Pattern.CASE_INSENSITIVE);
+    var matcher = pattern.matcher(acceptLangs);
+    var res = [];
+    while ( matcher.find() ) {
+      if ( res.indexOf( matcher.group() ) == -1 )
+        res.push( matcher.group() );
+    }
+    ***/
+
+    // Get accepted languages from header.
+    // en-CA,en;q=0.8,en-US;q=0.6,de-DE;q=0.4,de;q=0.2 → [en, de]
+    var re = /(^|;|,)([a-z]{2})/ig;
+    var found = false,
+        i = 0;
+    var res = [];
+
+    while ( found = re.exec(acceptLangs) ) {
+      if ( res.indexOf( found[2] ) == -1 )
+        res.push( found[2].toLowerCase() );
+    }
+
+    found = false;
+    while ( !found && i < res.length ) {
+      found = langMap[ res[i++] ];
+    }
+    lang = found ? found : langMap.default;
+  } else {
+    lang = countryMap[country];
+  }
+
+  return lang;
 }
