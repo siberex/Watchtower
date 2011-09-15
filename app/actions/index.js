9bbf1cafc81300c81b56d4875e44c7fa2f2f20f5
Stephan Legachev <me@sib.li>
2011-09-15 16:29:16 +0900

    Added language detection

--- app/actions/index.js
+++ app/actions/index.js
@@ -5,8 +5,16 @@
  */
 
 var {app} = require("../main");
-export("index");
+var {getLang} = require("../helpers");
+export("index", "test");
 
+function test(request) {
+
+  var context = {
+    lang    : getLang(request)
+  };
+  return app.render("test.html", context);
+} // test
 
 function index(request) {
   var context = {title: "Stepan Legachev salutes you!"};
