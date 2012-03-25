e5b7e07a46c50c085eee63565f5c3f97be3648b3
Stepan Legachev <siberex@gmail.com>
2012-03-25 14:27:13 +0900

    cleaned up middleware configuration

--- app/main.js
+++ app/main.js
@@ -12,10 +12,8 @@ var config = exports.config = require('./config');
 
 
 // Configure middleware.
-app.configure("session", "basicauth", "cookies", "params", "error", "notfound", "route", "mount", "render");
-//app.configure("session", "basicauth", "cookies", "notfound", "error", "params", "route", "mount", "render");
+app.configure("session", "basicauth", "cookies", "params", "notfound", "route", "mount", "render");
 
-app.error.template = module.resolve("views/500.html");
 app.notfound.template = module.resolve("views/404.html");
 
 // Configure render module.
@@ -36,21 +34,22 @@ jsgi.static( module.resolve("../pub") );
 var actions = require("./actions");
 
 
-// production environment, run with RINGO_ENV=production ringo demo.js
+// production environment, run with RINGO_ENV=production ringo main.js
 var prod = app.env("production");
 prod.configure("gzip", "etag", "error");
+prod.error.template = module.resolve("views/500.html");
 prod.error.location = false; // disable error location and stack traces
 
-// development environment, run with RINGO_ENV=development ringo demo.js
+// development environment, run with RINGO_ENV=development ringo main.js
 var dev = app.env("development").configure("requestlog", "error");
+dev.error.template = module.resolve("views/500.html");
 dev.requestlog.append = true;
 
-// profiler environment, run with RINGO_ENV=profiler ringo -o-1 demo.js
+// profiler environment, run with RINGO_ENV=profiler ringo -o-1 main.js
 var prof = app.env("profiler").configure("requestlog", "profiler", "error");
+prof.error.template = module.resolve("views/500.html");
 prof.requestlog.append = true;
 
-
-//app.error.location = false;
 
 // Start server if run as main script from ringo.
 // I hope no one will deploy it this way. Please use AppEngine instead.
