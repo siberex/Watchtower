93095940d589b744f4e73aec0f003d9c6faf176a
Stepan Legachev <siberex@gmail.com>
2012-03-26 06:52:01 +0900

    updated compile cmd-script to App Engine 1.6.3

--- _compile.cmd
+++ _compile.cmd
@@ -1,10 +1,10 @@
 @echo off
 
 REM "C:\Program Files\Java\jdk6\bin\javac.exe" -cp WEB-INF\lib\log4j-1.2.16.jar -d WEB-INF\classes -Xlint:all src\sibli\Pinger.java
-REM "C:\Program Files\Java\jdk6\bin\javac.exe" -cp WEB-INF\lib\appengine-api-1.0-sdk-1.6.4.jar -d WEB-INF\classes -Xlint:all -deprecation -g src\sibli\PingerAsync.java
+REM "C:\Program Files\Java\jdk6\bin\javac.exe" -cp WEB-INF\lib\appengine-api-1.0-sdk-1.6.3.1.jar -d WEB-INF\classes -Xlint:all -deprecation -g src\sibli\PingerAsync.java
 
 "C:\Program Files\Java\jdk6\bin\javac.exe" -cp "WEB-INF\lib\servlet-api.jar;WEB-INF\lib\log4j-1.2.16.jar" -d WEB-INF\classes src\sibli\PingTask.java
-"C:\Program Files\Java\jdk6\bin\javac.exe" -cp "WEB-INF\lib\appengine-api-1.0-sdk-1.6.4.jar;WEB-INF\lib\jdo2-api-2.3-eb.jar" -d WEB-INF\classes src\sibli\Host.java
+"C:\Program Files\Java\jdk6\bin\javac.exe" -cp "WEB-INF\lib\appengine-api-1.0-sdk-1.6.3.1.jar;WEB-INF\lib\jdo2-api-2.3-eb.jar" -d WEB-INF\classes src\sibli\Host.java
 
-REM "C:\Program Files\Java\jdk\bin\java.exe" -XX:-FailOverToOldVerifier -Xverify:all -cp WEB-INF\classes;WEB-INF\lib\appengine-api-1.0-sdk-1.6.4.jar sibli.PingerAsync
+REM "C:\Program Files\Java\jdk\bin\java.exe" -XX:-FailOverToOldVerifier -Xverify:all -cp WEB-INF\classes;WEB-INF\lib\appengine-api-1.0-sdk-1.6.3.1.jar sibli.PingerAsync
 @pause
