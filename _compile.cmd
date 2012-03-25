97e52d44d1b9c0c5e805b42355afdf36e85298e8
Stepan Legachev <siberex@gmail.com>
2012-03-26 04:30:10 +0900

    added logger to pingtask servlet updated logging configuration updated compile cmd script

--- _compile.cmd
+++ _compile.cmd
@@ -1,7 +1,9 @@
+@echo off
+
 REM "C:\Program Files\Java\jdk6\bin\javac.exe" -cp WEB-INF\lib\log4j-1.2.16.jar -d WEB-INF\classes -Xlint:all src\sibli\Pinger.java
-REM "C:\Program Files\Java\jdk6\bin\javac.exe" -cp WEB-INF\lib\appengine-api-1.0-sdk-1.6.2.1.jar -d WEB-INF\classes -Xlint:all -deprecation -g src\sibli\PingerAsync.java
+REM "C:\Program Files\Java\jdk6\bin\javac.exe" -cp WEB-INF\lib\appengine-api-1.0-sdk-1.6.4.jar -d WEB-INF\classes -Xlint:all -deprecation -g src\sibli\PingerAsync.java
 
-"C:\Program Files\Java\jdk6\bin\javac.exe" -cp WEB-INF\lib\servlet-api.jar -d WEB-INF\classes src\sibli\PingTask.java
+"C:\Program Files\Java\jdk6\bin\javac.exe" -cp "WEB-INF\lib\servlet-api.jar;WEB-INF\lib\log4j-1.2.16.jar" -d WEB-INF\classes src\sibli\PingTask.java
 
-REM "C:\Program Files\Java\jdk\bin\java.exe" -XX:-FailOverToOldVerifier -Xverify:all -cp WEB-INF\classes;WEB-INF\lib\appengine-api-1.0-sdk-1.6.2.1.jar sibli.PingerAsync
+REM "C:\Program Files\Java\jdk\bin\java.exe" -XX:-FailOverToOldVerifier -Xverify:all -cp WEB-INF\classes;WEB-INF\lib\appengine-api-1.0-sdk-1.6.4.jar sibli.PingerAsync
 @pause
--- src/sibli/PingTask.java
+++ src/sibli/PingTask.java
@@ -3,18 +3,21 @@ package sibli;
 import java.io.IOException;
 import javax.servlet.http.*;
 
+//import org.apache.log4j.Logger;
+import java.util.logging.Logger;
 
 /**
  * PingTask class.
  *
  */
 public class PingTask extends HttpServlet {
+  private static final Logger LOG = Logger.getLogger(PingTask.class.getName());
 
   public void doGet(HttpServletRequest req, HttpServletResponse resp)
   throws IOException {
 
 
-
+    LOG.info("Ololo!");
 
     /**
      * @todo Check for X-AppEngine-Cron: true request header.
