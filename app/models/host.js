ed17e836ac53d8fe8639cb18cc7e86809d9cf3c5
Stepan Legachev <siberex@gmail.com>
2012-06-10 05:03:25 +0900

    added new properties to model

--- app/models/host.js
+++ app/models/host.js
@@ -1,12 +1,13 @@
 var db = require('appengine/google/appengine/ext/db');
 
 var Host = exports.Host = db.Model('Host', {
-  url       : new db.LinkProperty({required: true}),
-  finalurl  : new db.LinkProperty( {DEFAULT: null} ),
-  domain    : new db.StringProperty( {DEFAULT: null} ),
+  url       : new db.LinkProperty(    {required: true}),
+  finalurl  : new db.LinkProperty(    {defaultValue: null} ),
+  domain    : new db.StringProperty(  {defaultValue: null} ),
   added     : new db.DateTimeProperty({autoNowAdd: true}),
   updated   : new db.DateTimeProperty({autoNowAdd: true}),
-  status    : new db.IntegerProperty(),
-  viewed    : new db.DateTimeProperty(),
-  useget    : new db.BooleanProperty( {DEFAULT: false} ) // Use GET method instead of HEAD
+  status    : new db.IntegerProperty( {required: true}),
+  viewed    : new db.DateTimeProperty({defaultValue: null}),
+  views     : new db.IntegerProperty( {defaultValue: 0} ),
+  useget    : new db.BooleanProperty( {defaultValue: false} ) // Use GET method instead of HEAD
 });
