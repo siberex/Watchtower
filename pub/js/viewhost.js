9863bba2cc472705cafd3e2093a9af2a984d20cd
Stepan Legachev <siberex@gmail.com>
2012-06-11 07:30:11 +0900

    updated stock view

--- pub/js/viewhost.js
+++ pub/js/viewhost.js
@@ -1,17 +1,6 @@
 $(function() {
     $.getJSON(dataUrl, function(data) {
 
-        var flags = [];
-        for (var i in data) {
-            if (data[i].s != 200) {
-                flags.push({
-                    x: data[i].x,
-                    y: data[i].s,
-                    title: data[i].s
-                });
-            }
-        }
-
         // Create the chart
         window.chart = new Highcharts.StockChart({
             chart : {
@@ -25,11 +14,26 @@ $(function() {
                 text : hostUrl
             },
             yAxis: {
-                title : 'Response time (ms)',
+                title : 'Response time (ms)'
+                //,max: 5000
             },
             rangeSelector : {
-                enabled: false,
-                selected : 2
+                enabled: true,
+                inputEnabled: false,
+                buttons: [{   //  'millisecond', 'second', 'minute', 'day', 'week', 'month', 'ytd' (year to date), 'year' and 'all'.
+                    type: 'day',
+                    count: 1,
+                    text: '1d'
+                }, {
+                    type: 'week',
+                    count: 1,
+                    text: '1w'
+                }, {
+                    type: 'month',
+                    count: 1,
+                    text: '1m'
+                }],
+                selected : 1
             },
 
             scrollbar : {
@@ -41,26 +45,30 @@ $(function() {
                 crosshairs  : true,
                 formatter   : function() {
                     return Highcharts.dateFormat('%d.%m %H:%M', this.x) +':<br/>' + this.series.name + ': '
-                         + '<b>' + parseInt(this.y) + '</b>'
-                         + (this.point.s ? '<br />Status: ' + '<b>' + this.point.s + '</b>' : '');
+                         + '<span style="font-weight:bold;color:' + (this.y<='2000'?'black':'red') + '">' + parseInt(this.y) + '</span> ms'
+                         + (this.point.s ? '<br />Status: ' + '<span style="font-weight:bold;color:' + (this.point.s=='200'?'black':'red') + '">' + this.point.s + '</span>' : '');
                 }
             },
 
             series : [{
-                name    : "Response time (ms)",
+                name    : "Response time",
                 data    : data,
+                type: 'areaspline',
                 id: 'dataseries',
+                fillColor : {
+                    linearGradient : {
+                        x1: 0,
+                        y1: 0,
+                        x2: 0,
+                        y2: 1
+                    },
+                    stops : [[0, Highcharts.getOptions().colors[0]], [1, 'rgba(0,0,0,0)']]
+                },
+                lineWidth : 0, // 1
                 marker  : {
                     enabled : true,
                     radius  : 2
-                },
-                shadow  : true
-            }, {
-                type    : 'flags',
-                name    : 'Code',
-                data    : flags,
-                onSeries: 'dataseries',
-                shape   : 'squarepin'
+                }
             }]
         });
     });
