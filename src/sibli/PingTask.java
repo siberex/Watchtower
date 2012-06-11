935f3bfc8c9122695b286f503bf8bd81ca46896b
Stepan Legachev <siberex@gmail.com>
2012-06-11 21:56:15 +0900

    removed comments

--- src/sibli/PingTask.java
+++ src/sibli/PingTask.java
@@ -107,24 +107,11 @@ public class PingTask extends HttpServlet {
 
 
 /**
-@todo Handle big resultset warning on host view:
-com.google.appengine.api.datastore.QueryResultsSourceImpl logChunkSizeWarning
-WARNING: This query does not have a chunk size set in FetchOptions and has returned over 1000 results.
-If result sets of this size are common for this query, consider setting a chunk size to improve performance.
-To disable this warning set the following system property in appengine-web.xml (the value of the
-property doesn't matter): 'appengine.datastore.disableChunkSizeWarning'
-
-
 @todo We can implement usage for getRemainingMillis() in case of long-running task.
 import  com.google.apphosting.api.ApiProxy;
 long ApiProxy.getCurrentEnvironment().getRemainingMillis();
 https://developers.google.com/appengine/docs/java/runtime#The_Request_Timer
 
-Cursors:
-https://developers.google.com/appengine/docs/java/datastore/queries#Query_Cursors
-
-@todo Check environment to automatically distinct backend from frontend and set appropriate limits
-https://developers.google.com/appengine/docs/java/javadoc/com/google/apphosting/api/ApiProxy.Environment
 
 @todo Optimize write costs:
 http://code.google.com/appengine/docs/billing.html#Billable_Resource_Unit_Cost
@@ -135,16 +122,7 @@ Even without custom indexes it will take 47088 writes to save 3924 entities 5 pr
 (12 write ops per entity = 2w + 2wpi*5props).
 Each HostQuery entity = 10 write ops.
 
-AppStats can be used for profiling:
-https://developers.google.com/appengine/docs/java/tools/appstats?hl=ru-RU
-
 @todo Save status and time properties as unindexed (?):
 http://stackoverflow.com/questions/7318591/how-does-google-appengine-measure-datastore-put-operations
-
-
-READ THIS TWICE A DAY!
-http://googleappengine.blogspot.com/2009/06/10-things-you-probably-didnt-know-about.html
-Especially about batch operations:
-https://developers.google.com/appengine/docs/java/datastore/entities#Batch_Operations
 
 */
